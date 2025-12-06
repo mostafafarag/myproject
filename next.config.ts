347ec811092f1ffa5235211b21f94e331728dc34
Mostafa Farag <mostafafarag15@yahoo.com>
2025-12-06 13:05:14 +0200

    Fix Next.js 15 eslint config error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
-};
-
-export default nextConfig;
+module.exports = nextConfig;
